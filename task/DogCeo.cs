e870c205af8ba65213a7a3ae8f3dd3e425932816
Emohamed <mhmoed8000900@gmail.com>
2025-10-04 21:57:34 +0200

    Add project files.

---
+++ task/DogCeo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+
+namespace TaskAPI
+{
+    public class DogCeo
+    {
+        //Singleton
+        private static DogCeo instance;
+        private static readonly object _lock = new object();
+        private HttpClient _httpClient = new HttpClient();
+
+        //Observer
+        private List<Action<string>> _observers = new List<Action<string>>();
+
+        private DogCeo() { }
+
+        public static DogCeo Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (_lock)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new DogCeo();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+
+
+
+
+        public void AddObsrever(Action<string> observer)
+        {
+            _observers.Add(observer);
+        }
+        //تنبيه المراقبين Api من url دالة جلب
+
+
+
+        public async Task FetchDogImageAsync()
+        {
+            string url = "https://dog.ceo/api/breeds/image/random";
+            string json = await _httpClient.GetStringAsync(url);
+            dynamic result = JsonConvert.DeserializeObject(json);
+            string ImageUrl = result.message;
+
+            foreach (var observer in _observers)
+            {
+                observer(ImageUrl);
+            }
+        }
+       
+    }
+}
---
+++ task/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using TaskAPI;
+
+public class Program
+{
+    public static async Task Main()
+    {
+        var manager = DogCeo.Instance;
+
+        manager.AddObsrever(url => Console.WriteLine(" Dog image Url 1: " + url));
+        manager.AddObsrever(url => Console.WriteLine(" Dog image Url 2: " + url));
+
+        Console.WriteLine(" Fetching dog image.");
+        await manager.FetchDogImageAsync();
+    }
+}
